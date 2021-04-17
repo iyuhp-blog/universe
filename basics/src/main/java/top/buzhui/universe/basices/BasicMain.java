533e664d29beaae57bc4328900983af392c89864
peifeng <ixiaopingyu@aliyun.com>
2021-04-17 10:35:01 +0800

    first init

---
+++ basics/src/main/java/top/buzhui/universe/basices/BasicMain.java
@@ -0,0 +1,7 @@
+package top.buzhui.universe.basices;
+
+/**
+ * @author iyuhp created at 2021/4/17
+ */
+public class BasicMain {
+}
---
+++ basics/src/test/java/top/buzhui/universe/basics/BasicMainTest.java
@@ -0,0 +1,17 @@
+package top.buzhui.universe.basics;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author hexiao
+ * @date 2021/4/17
+ */
+public class BasicMainTest {
+
+
+    @Test
+    public void helloTest() {
+        System.out.println("Hello hexiao, hello universe");
+    }
+
+}
